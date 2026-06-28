171bfb4522e9b2d5eeddd68f29c96654e82ca269
DavidWhelan1890 <108284677+DavidWhelan1890@users.noreply.github.com>
2026-06-28 16:36:39 -0500

    Add build and update .gitignore

--- WebTemplateFss.GeneratedStyles/WebTemplateFss.GeneratedStyles.fs
+++ WebTemplateFss.GeneratedStyles/WebTemplateFss.GeneratedStyles.fs
@@ -1,4 +1,18 @@
 namespace WebTemplateFss.GeneratedStyles
 
+module styles =
+    let container = "css-848858348" 
+    let header = "css-1813318290" 
+    let input = "css1878228952InputClass" 
+    let fadeAnimation = "animation-1895258194" 
+    let button = "css1793788467" 
+    let indexCounter = "counter0indexCounter" 
+    let counter = "css-775622825" 
+    let counterDone = "css1504904407" 
+
 module fonts =
     let DroidSerifFontFace = "DroidSerifFontFace" 
+    let modernaFontFace = "modernaFontFace" 
+    let droidSerif = "css1483649991DroidSerif" 
+    let droidSerifBold = "css380929670DroidSerifBold" 
+    let moderna = "css173652974Moderna" 
